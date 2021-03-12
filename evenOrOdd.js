9e419fa8ada26329c99a2b62502d9d24d4f40cf8
Oleg Chachin <44916575+WalkingFrozenFish@users.noreply.github.com>
2021-03-12 14:53:34 +0600

    Add files via upload

---
+++ evenOrOdd.js
@@ -0,0 +1,9 @@
+// Kata 13 - Even or Odd
+function even_or_odd(number) {
+    return number % 2 == 0 ? "Even" : "Odd";
+}
+
+even_or_odd(2); // "Even"
+even_or_odd(0); // "Even"
+even_or_odd(7); // "Odd"
+even_or_odd(1); // "Odd"
